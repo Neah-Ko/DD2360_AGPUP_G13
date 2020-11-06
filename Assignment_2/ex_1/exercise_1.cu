2851c63a12b1b4a23672d1dfb7b8a51129a74836
Etienne Jodry <etienne.jodry@hotmail.fr>
2020-11-06 13:55:39 +0100

    ex_1 corrections

--- Assignment_2/ex_1/exercise_1.cu
+++ Assignment_2/ex_1/exercise_1.cu
@@ -1,15 +1,17 @@
 #include <stdio.h>
 
+#define TPB 256
+#define NB 1
 
 __global__ void helloKernel(){
+	/* not necessary since we have 1 block */
 	// const int i = blockIdx.x * blockDim.x + threadIdx.x;
 	printf("Hello World! My threadId is %d\n", threadIdx.x);
 }
 
 
-
 int main(){
-	helloKernel<<<256, 1>>>();
+	helloKernel<<<NB, TPB>>>();
 	cudaDeviceSynchronize();
 	return 0;
 }
