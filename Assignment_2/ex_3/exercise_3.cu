d9b8406999fa5b04461255d353255beb56a67978
DarkKapla <DarkKapla@users.noreply.github.com>
2020-11-12 12:36:06 +0100

    Add measurement for data copying time in A2-e3

--- Assignment_2/ex_3/exercise_3.cu
+++ Assignment_2/ex_3/exercise_3.cu
@@ -44,23 +44,27 @@ __global__ void timestepKernel(size_t n, int t, Particle * d_vec){
 	);
 }
 
-void simulationLauncher(size_t n, int m, int tpb, Particle * vec){
+double simulationLauncher(size_t n, int m, int tpb, Particle * vec){
 	Particle * d_vec;
 	
 
 	cudaMalloc(&d_vec, n*sizeof(Particle));
 
-
+	double cpy_iStart1 = cpuSecond();
 	cudaMemcpy(d_vec, vec, n*sizeof(Particle), cudaMemcpyHostToDevice);
-
+	double cpy_iElaps1 = cpuSecond() - cpy_iStart1;
 
 	for(int t = 0; t < m; t++){
 		timestepKernel<<<(n + tpb - 1)/tpb, tpb>>>(n, t, d_vec);
 	}
 
+	double cpy_iStart2 =cpuSecond();
 	cudaMemcpy(vec, d_vec, n*sizeof(Particle), cudaMemcpyDeviceToHost);
+	double cpy_iElaps2 = cpuSecond() - cpy_iStart2;
 
 	cudaFree(d_vec);
+
+	return cpy_iElaps1 + cpy_iElaps2;
 }
 
 void timestep(size_t n, int t, Particle * vec){
@@ -125,11 +129,13 @@ int main(int argc, char **argv){
 	/* simulate: GPU */
 	printf("Simulating on the GPU… ");
 	double gpu_iStart = cpuSecond();
-	simulationLauncher(n, m, tpb, gVec);
+	double cpy_time = simulationLauncher(n, m, tpb, gVec);
 	cudaDeviceSynchronize();
 	double gpu_iElaps = cpuSecond() - gpu_iStart;
 	printf("Done! in %f seconds\n", gpu_iElaps);
 
+	/* print some data on stderr to record the data copying time. */
+	fprintf(stderr, "%d %d %lf\n", n, tpb, cpy_time);
 
 	/* Compare */
 	printf("Comparing the output for each implementation… ");
@@ -143,5 +149,6 @@ int main(int argc, char **argv){
 		return -1;
 	}
 	printf("Correct!\n");
+
 	return 0;
 }
