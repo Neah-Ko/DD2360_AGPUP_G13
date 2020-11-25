58bca11465af923409647c8c803770405ffce4f3
Etienne Jodry <etienne.jodry@hotmail.fr>
2020-11-25 12:58:58 +0100

    Code for ex_bonus finished

--- Assignment_3/ex_bonus/exercise_bonus.cu
+++ Assignment_3/ex_bonus/exercise_bonus.cu
@@ -132,7 +132,10 @@ void shared_sgemm_kernel(float *C, float *A, float *B, long size)
 	const long row = blockIdx.y * blockDim.y + threadIdx.y;
 	float val = 0.0;
 
-	/* TODO declare shared memory with size TILE_SIZE x TILE_SIZE */
+	/* DONE: declare shared memory with size TILE_SIZE x TILE_SIZE */
+	__shared__ float tile_A[TILE_SIZE][TILE_SIZE];
+	__shared__ float tile_B[TILE_SIZE][TILE_SIZE];
+
 
 	if (col < size && row < size) {
 		const long local_col = blockIdx.x * TILE_SIZE + threadIdx.x;
@@ -143,9 +146,11 @@ void shared_sgemm_kernel(float *C, float *A, float *B, long size)
 			tile_B[threadIdx.y][threadIdx.x] = B[(m * TILE_SIZE + threadIdx.y) * size + local_col];
 			__syncthreads();
 	
-			/* TODO introduce a pragma directive that can potentially improve performance here */
+			/* DONE: introduce a pragma directive that can potentially improve performance here */
+			#pragma unroll
 			for (long k = 0; k < TILE_SIZE; ++k) {
-				/* TODO Perform multiplication here */
+				/* DONE: Perform multiplication here */
+				val += tile_A[threadIdx.y][k] * tile_B[k][threadIdx.x];
 			}
 			__syncthreads();
 		}
@@ -178,8 +183,11 @@ void cublas_sgemm(float *C, float *A, float *B, long size)
 	cublasCreate(&handle);
 
 	gettimeofday(&t0, NULL);
-	/* TODO fill in the blanks, do C = BA instead of C = AB */
-	cublasSgemm(handle, CUBLAS_OP_N, CUBLAS_OP_N, , , , , , , , , , , );
+	/* DONE: fill in the blanks, do C = BA instead of C = AB */
+	// cublasSgemm(handle, CUBLAS_OP_N, CUBLAS_OP_N, , , , , , , , , , , );
+	//                                            h_A   w_B   w_A              w_B      w_A             w_A  
+	cublasSgemm(handle, CUBLAS_OP_N, CUBLAS_OP_N, size, size, size, &alpha, B, size, A, size, &beta, C, size);
+
 	checkCudaErrors(cudaDeviceSynchronize());
 	gettimeofday(&t1, NULL);
 	cublasDestroy(handle);
